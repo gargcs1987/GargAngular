fbc0794d1aecfd762f4991d1d2c2ab46b0e24d15
Garg <gargcs1987@yahoo.com>
2018-05-06 11:40:07 +0530

    First Commit

---
+++ sample.js
@@ -0,0 +1,17 @@
+
+var sqr=4*4;
+
+console.log(sqr);
+
+console.log(sqr);
+
+var result=function(num)
+{
+    return num*num;
+}
+
+console.log(result);
+console.log(result(5));
+
+console.log('amit garg');
+
